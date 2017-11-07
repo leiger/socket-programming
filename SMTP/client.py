e08fc6b3e9e5bada065ad4b40e2f99e56105a06b
leiger <leiger@bupt.edu.cn>
2017-11-07 16:40:34 -0500

    v 1.1

--- SMTP/client.py
+++ SMTP/client.py
@@ -1,5 +1,7 @@
 from socket import *
 import ssl
+import base64
+import time
 
 msg = "\r\n I love computer networks!"
 endmsg = "\r\n.\r\n"
@@ -9,7 +11,6 @@ mailserver = ('smtp.gmail.com', 587)
 
 # Create socket called clientSocket and establish a TCP connection with mailserver
 clientSocket = socket(AF_INET, SOCK_STREAM)
-# clientSocket = so
 clientSocket.connect(mailserver)
 
 recv = clientSocket.recv(1024).decode()
@@ -25,33 +26,56 @@ print(recv1)
 if recv1[:3] != '250':
     print('250 reply not received from server.')
 
+
+tls = "STARTTLS\r\n".encode()
+clientSocket.send(tls)
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+clientSocket = ssl.wrap_socket(clientSocket)
+
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024)
+recv1 = recv1.decode()
+print(recv1)
+
+#username and password
+username = input("USERNAME:")
+password = input("PASSWORD:")
+base64_str = ('\x00' + username + '\x00' + password).encode()
+base64_str = base64.b64encode(base64_str)
+base64_str = base64_str.strip("\n".encode())
+#auth = base64.b64encode(base64_str)
+authMsg = "AUTH PLAIN ".encode()+base64_str+"\r\n".encode()
+clientSocket.send(authMsg)
+recv1 = clientSocket.recv(1024)
+print("After auth: "+recv1.decode())
+
+
+
 # Send MAIL FROM command and print server response.
-clientSocket.send('MAIL FROM: <leiger2017@gmail.com>\r\n'.encode())
+clientSocket.send('MAIL FROM:<leiger2017@gmail.com>\r\n'.encode())
 recv1 = clientSocket.recv(1024).decode()
 print('MAIL:', recv1)
 if recv1[:3] != '250':
     print('mail 250 reply not received from server.')
 
 # Send RCPT TO command and print server response.
-clientSocket.send('RCPT TO:<leiger2017@gmail.com>\r\n'.encode())
+clientSocket.send('RCPT TO:<1120720058@qq.com>\r\n'.encode())
 recv1 = clientSocket.recv(1024).decode()
 print('RCPT:', recv1)
 if recv1[:3] != '250':
     print('rcpt 250 reply not received from server.')
 
 # Send DATA command and print server response.
-clientSocket.send("DATA\r\n".encode())
+clientSocket.send('DATA\r\n'.encode())
 recv1 = clientSocket.recv(1024).decode()
 print('DATA:', recv1)
-if recv1[:3] != '250':
-    print('data 250 reply not received from server.')
+if recv1[:3] != '354':
+    print('data 354 reply not received from server.')
 
 # Send message data.
+clientSocket.send('Subject: hahaha\r\n\r\n'.encode())
 clientSocket.send(msg.encode())
-recv1 = clientSocket.recv(1024).decode()
-print('MSG:', recv1)
-if recv1[:3] != '354':
-    print('message 250 reply not received from server.')
 
 # Message ends with a single period.
 clientSocket.send(endmsg.encode())
